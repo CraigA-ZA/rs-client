fbdd2b838107d27384329d8c490dfc55569d0441
thor <torstenprejawa@gmail.com>
2023-04-08 16:44:36 +0200

    Use file separator, allowing use on different operating systems. Use user.dir to get the root directory of the project

--- shared-utils/src/main/java/za/org/secret/Constants.java
+++ shared-utils/src/main/java/za/org/secret/Constants.java
@@ -2,6 +2,7 @@ package za.org.secret;
 
 import java.awt.*;
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Constants {
@@ -25,8 +26,8 @@ public class Constants {
     public static final String INITIAL_JAR = "initial_jar";
     public static final String OUTPUT_FILE_NAME = "gamepack.jar";
     public static final String INITIAL_CLASS = "initial_class";
-    public static final String GAMEPACK_OUTPUT_DIR = Paths.get(System.getProperty("user.home"), "gamepack") + "\\";
-    public static final String DEOB_OUTPUT_JAR_PATH = Paths.get(System.getProperty("user.home"), "gamepack") + "\\" + "deob_gamepack.jar";
+    public static final String GAMEPACK_OUTPUT_DIR = Paths.get(System.getProperty("user.dir"), "gamepack").toString();
+    public static final String DEOB_OUTPUT_JAR_PATH = Paths.get(System.getProperty("user.dir"), "gamepack") + File.separator+ "deob_gamepack.jar";
 
-    public static final String DEOB_IN_PROGRESS_PATH = Paths.get(System.getProperty("user.home"), "gamepack") + "\\" + "deob_gamepack.jar.part";
+    public static final String DEOB_IN_PROGRESS_PATH = Paths.get(System.getProperty("user.dir"), "gamepack") + File.separator + "deob_gamepack.jar.part";
 }
